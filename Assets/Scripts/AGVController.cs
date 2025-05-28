dbec11406161607e73fb0e161cb795dd833a0cb5
qqAC3R <idenisden025@gmail.com>
2025-05-28 16:55:21 +0200

    Started implementing synchronization for the robot using /odom topic

--- Assets/Scripts/AGVController.cs
+++ Assets/Scripts/AGVController.cs
@@ -41,7 +41,7 @@ namespace RosSharp.Control
             SetParameters(wA1);
             SetParameters(wA2);
             ros = ROSConnection.GetOrCreateInstance();
-            ros.Subscribe<TwistMsg>("cmd_vel", ReceiveROSCmd);
+            ros.Subscribe<TwistMsg>("/cmd_vel", ReceiveROSCmd);
         }
 
         void ReceiveROSCmd(TwistMsg cmdVel)
